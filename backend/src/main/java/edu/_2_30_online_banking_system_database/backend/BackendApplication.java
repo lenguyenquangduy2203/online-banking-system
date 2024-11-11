7f14055102b973de697d0bed273a2c68f15fb14a
heineken <lenguyenquanduy@gmail.com>
2024-11-11 19:52:46 +0700

    Initiate online banking project

---
+++ backend/src/main/java/edu/_2_30_online_banking_system_database/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package edu._2_30_online_banking_system_database.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/_2_30_online_banking_system_database/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu._2_30_online_banking_system_database.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
