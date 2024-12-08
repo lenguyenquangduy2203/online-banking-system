5c9c1bcecb65b250bc3d277e8aea09dfe16e5893
heineken <lenguyenquanduy@gmail.com>
2024-12-08 09:24:54 +0700

    Adding CORS annotation for controllers

--- backend/src/main/java/edu/_2_30_online_banking_system_database/backend/controllers/UserController.java
+++ backend/src/main/java/edu/_2_30_online_banking_system_database/backend/controllers/UserController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import edu._2_30_online_banking_system_database.backend.services.UserAccountServ
 import lombok.AllArgsConstructor;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path = "/api/users")
 @AllArgsConstructor
 public class UserController {
